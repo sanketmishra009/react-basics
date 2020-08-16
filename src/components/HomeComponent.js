08f12051f451715659479bef0b2d9208bb60a2d3
sanket mishra <sanketmishra@protonmail.com>
2020-08-16 16:18:34 +0530

    Single Page Application part-1

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import { Card, CardImg, CardBody, CardTitle, CardSubtitle, CardText } from 'reactstrap';
+const RenderCard = ({item}) =>{
+    return(
+        <Card>
+            <CardImg src={item.image} alt={item.name}/>
+            <CardBody>
+                <CardTitle><h1>{item.name}</h1></CardTitle>
+                {item.designation ? <CardSubtitle><h2>{item.designation},</h2></CardSubtitle> : null }
+                <CardText>{item.description}</CardText>
+            </CardBody>
+        </Card>
+    );
+}
 function Home(props){
     return(
         <div className="container">
-            <h4>Home</h4>
+            <div className='row align-items-start'>
+                <div className='col-sm col-md m-1'>
+                    <RenderCard item= {props.dish}/>
+                </div>
+                <div className='col-sm col-md m-1'>
+                    <RenderCard item= {props.promotion}/>
+                </div>
+                <div className='col-sm col-md m-1'>
+                    <RenderCard item= {props.leader}/>
+                </div>
+            </div>
         </div>
     );
 }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,31 +1,43 @@
 import React ,{Component} from 'react';
 import Menu from "./MenuComponent";
-import {DISHES} from '../shared/dishes'
+//data imports from shared directory.
+import {DISHES} from '../shared/dishes';
+import {PROMOTIONS} from '../shared/promotions';
+import {LEADERS} from '../shared/leaders';
+import {COMMENTS} from '../shared/comments';
+//end of data exports.
 import { render } from '@testing-library/react';
 import Dishdetail from './DishdetailComponent';
 import Header from './Header';
 import Footer from './Footer';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import {Switch,Route,Redirect} from 'react-router-dom'; 
 
 class Main extends Component{
   constructor(props){
     super(props);
-    this.state={dishes:DISHES
+    this.state={dishes:DISHES,
+      comments:COMMENTS,
+      leaders:LEADERS,
+      promotions:PROMOTIONS
     };
   };
   render() {
     const Homepage = ()=>{
       return (
-        <Home/>
+        <Home dish={this.state.dishes.filter((dish)=> dish.featured )[0]}
+        promotion={this.state.promotions.filter((promotion)=> promotion.featured )[0]}
+        leader={this.state.leaders.filter((leader)=> leader.featured )[0]}/>
       );
     }
     return (
       <div>
         <Header/>
         <Switch>
-          <Route path='/home' component={()=><Home/>}/>
+          <Route path='/home' component={()=>Homepage()}/>
           <Route exact path = '/menu' component={() => <Menu dishes={this.state.dishes}/>}/>
+          <Route exact path ='/contact' component={Contact}/>
           <Redirect to='/home'/>
         </Switch>
         <Footer/>
