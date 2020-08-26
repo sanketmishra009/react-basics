09601f944a3900c00dce6d90584fd4b019eb1c2f
sanket mishra <sanketmishra@protonmail.com>
2020-08-26 15:10:29 +0530

    Comment component in Dish-detail component.

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React,{Component} from 'react';
 import {Card, CardImg, CardImgOverlay,CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button} from 'reactstrap';
 import {Link} from 'react-router-dom';
-import SubmitComment from './CommentComponent';
+import {Control, LocalForm, Errors} from 'react-redux-form';
+import { Modal, ModalHeader, ModalBody, Form,FormGroup, Label,Input,Row,Col} from 'reactstrap';
 
     function RenderDish({dish}){
         if (dish != null){
@@ -72,5 +73,78 @@ import SubmitComment from './CommentComponent';
                 </div>
             </div>
         );
+    }
+    const required = (val) => val && val.length;
+    const maxLength = (len) => (val) => !(val) || (val.length <= len);
+    const minLength = (len) => (val) => val && (val.length >= len);
+    class SubmitComment extends Component{
+        constructor(props){
+            super(props);
+            this.state={
+                isModalOpen : false
+            }
+            this.toggleModal = this.toggleModal.bind(this);
+        }
+        toggleModal(){
+            this.setState({
+                isModalOpen : !this.state.isModalOpen
+            });
+        }
+        render(){
+            return(
+                <>
+            <Button outline onClick={this.toggleModal}>
+                <span className = 'fa fa-pencil fa-lg'></span>Submit Comments</Button>
+            <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                <ModalHeader>SubmitComment</ModalHeader>
+                <ModalBody>
+                    <LocalForm>
+                        <Row className='form-group'>
+                            <Col>
+                                <Label>Rating:</Label>
+                                <Control.select model=".rating" name="rating" className="form-control">
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                </Control.select>
+                            </Col>
+                        </Row>
+                        <Row className='form-group'>
+                            <Col>
+                                <Label htmlFor='yourname'>Your Name:</Label>
+                                <Control.text model='.yourname' id='yourname' placeholder = 'Your Name' className='form-control' 
+                                validators={{
+                                    required, minLength: minLength(3), maxLength: maxLength(15)
+                                }}  />
+                                <Errors
+                                            className="text-danger"
+                                            model=".yourname"
+                                            show="touched"
+                                            messages={{
+                                                required: 'Required',
+                                                minLength: 'Must be greater than 2 characters',
+                                                maxLength: 'Must be 15 characters or less'
+                                            }}
+                                         />
+                            </Col>
+                        </Row>
+                        <Row className='form-group'>
+                            <Col>
+                                <Label htmlFor='comment'>Comment:</Label>
+                                <Control.textarea model='.comment' id='comment' name='comment' className='form-control' rows='6'ß/>
+                            </Col>
+                        </Row>
+                        <Row className="form-group">
+                            <Col>
+                                <Button type='submit' color='primary'>Submit</Button>
+                            </Col>
+                        </Row>
+                    </LocalForm>
+                </ModalBody>
+            </Modal>
+            </>
+            );
+        }
     }
 export default Dishdetail;
